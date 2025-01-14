9395a779d93f589aa74df7da2381b8fd207ce177
monarulMN <mdmonarulislam720@gmail.com>
2025-01-14 12:49:06 +0600

    First Commit

---
+++ Data/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using ToDoTask.Models;
+
+namespace ToDoTask.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<ToDoTask.Models.Task> Task { get; set; } = default!;
+    }
+}
---
+++ Models/Task.cs
@@ -0,0 +1,9 @@
+﻿namespace ToDoTask.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
